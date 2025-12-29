ce6766b45fd5adc82580f81eec15fb901680ef38
Alejo Arenas Moreira <arenasmoreira@gmail.com>
2025-12-29 17:00:11 +0100

    fix on bootstrap includes on missing file

--- api/actions/calculadora/deleteVersion.php
+++ api/actions/calculadora/deleteVersion.php
@@ -2,7 +2,7 @@
 
     //PHP para borrar una versión de un presupuesto concreto
 
-    $pdo = require_once('../../../config/bootstrap.php');
+    $pdo = require_once __DIR__ . '/../../../config/bootstrap.php';
 
     //CONSULTA A LA BASE DE DATOS USANDO EL PDO
     $cod_oferta = limpiar_dato($_GET['id']);
