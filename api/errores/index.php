f2751109a6a55deba2f9929e3e1fba3e9d44f89e
Alejo Arenas Moreira <arenasmoreira@gmail.com>
2026-01-03 00:36:26 +0100

    BUGFIX: error page back icon

--- api/errores/index.php
+++ api/errores/index.php
@@ -24,7 +24,7 @@ require_once BASE_DIR . "/api/templates/head.php";
         404 - Página no encontrada
       </h3>
       <a id="volver" href="<?= $_SERVER['HTTP_REFERER'] ?? BASE_URL . '/index.php' ?>">
-        <img src="<?= BASE_URL ?>/public/img/back.svg" alt="Volver" class="flecha">
+        <svg class="flecha" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 16 16"><path fill="#f0eeee" d="m3.825 9 5.6 5.6L8 16 0 8l8-8 1.425 1.4-5.6 5.6H16v2z"/></svg>
         <span>Volver</span>
       </a>
     </div>
